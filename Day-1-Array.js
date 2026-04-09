25fddf20509f3028b754d3dacace83e623161c25
Kishan Kewal <iamdevildeveloper@gmail.com>
2026-04-09 10:51:36 +0530

    Practiced with array questions. Easy

---
+++ Day-1-Array.js
@@ -0,0 +1,53 @@
+// 1. Find maximum
+// let input = [3, 7, 2, 9, 5];
+// let maxValue = 0;
+
+// for(let i = 0; i < input.length; i++){
+//     if(maxValue < input[i]){
+//         maxValue = input[i];
+//     }
+// }
+// console.log(maxValue); // 9
+
+// 2. Find Minimum Element
+// let input = [1, 2, -11, 3, 0, 4, -9];
+// let minValue = null;
+
+// for(let i = 0; i < input.length; i++){
+//     if(minValue == null || minValue > input[i]){
+//         minValue = input[i];
+//     }
+// }
+// console.log(minValue); // -11
+
+// 3. Sum of Array
+// let input = [1, 2, 3, 4];
+// let sum = 0;
+
+// for(let i = 0; i < input.length; i++){
+//     sum += input[i];
+// }
+// console.log(sum); // 10
+
+// 4. Count Even Numbers
+// let input = [1, 2, 3, 4, 6, 7, 8];
+// let evenCount = 0;
+// for(let i = 0; i < input.length; i++){
+//     if((input[i] % 2) == 0){
+//         evenCount++;
+//     }
+// }
+// console.log(evenCount); // 4
+
+// 5. Reverse an Array (NO .reverse())
+// let input = [1, 2, 3, 4, 6, 7, 8];
+// let reversed = [];
+// let count = 0;
+// for(let i = 0; i < input.length; i++){
+//     const reversedVal = input[input.length - count++];
+//     if(reversedVal !== undefined) {
+//         reversed.push(reversedVal);
+//     } 
+// }
+// console.log(reversed);
+
