2f93c339877374aa1ffb03458f542314b89c3a51
Kishan Kewal <iamdevildeveloper@gmail.com>
2026-04-11 07:36:28 +0530

    Practiced arrays

--- Day-1-Array.js
+++ Day-1-Array.js
@@ -127,3 +127,103 @@
 // console.log(uniqueArray);
 
 // 9. Move All Zeros to End
+// let input = [0, 1, 0, 3, 12];
+// let notZeroArray = [];
+// let zeroArray = [];
+
+// for(let i = 0; i < input.length; i++){
+//     if(input[i] != 0){
+//         notZeroArray.push(input[i]);
+//     } else {
+//         zeroArray.push(input[i]);
+//     }
+// }
+
+// for(let x = 0; x < zeroArray.length; x++){
+//     notZeroArray.push(zeroArray[x]);
+// }
+
+// console.log(notZeroArray);
+
+// 10. Rotate Array by 1 (Right)
+// let input = [1, 2, 3, 4];
+// let otherValues = [];
+// let updatedArray = [];
+// for(let i = 0; i < input.length; i++){
+//     if(i < input.length - 1){
+//         otherValues.push(input[i]);
+//     }
+// }
+// updatedArray.push(input[input.length-1]);
+// for(let i = 0; i < otherValues.length; i++){
+//     updatedArray.push(otherValues[i]);
+// }
+
+// console.log(updatedArray);
+
+// 11. Find Missing Number (0 to n)
+// function doesValueExistInArray(value, array){
+//     let result = false;
+//     for(let i = 0; i < array.length; i++){
+//         if(value == array[i]){
+//             result = true;
+//         }
+//     }
+//     return result;
+// }
+// let input = [0,1,3,10];
+// let lastNumberOfArray = input[input.length-1];
+// let missingNumbers = [];
+// for(let i = 0; i < lastNumberOfArray; i++){
+//     if(doesValueExistInArray(i, input) == false){
+//         missingNumbers.push(i)
+//     }
+// }
+// console.log(missingNumbers);
+
+
+// 12. Find Duplicate Number
+// function doesValueExistInArray(value, array){
+//     let result = false;
+//     for(let i = 0; i < array.length; i++){
+//         if(value == array[i]){
+//             result = true;
+//         }
+//     }
+//     return result;
+// }
+
+// let input = [1, 3, 4, 2, 2];
+// let duplicates = [];
+// let uniqueArray = [];
+// for(let i = 0; i < input.length; i++){
+//     if(uniqueArray.length == 0){
+//         uniqueArray.push(input[i]);
+//     } else {
+//         for(let x = 0; x < uniqueArray.length; x++){
+//             // console.log(input[i], uniqueArray, doesValueExistInArray(input[i], uniqueArray))
+//             if(doesValueExistInArray(input[i], uniqueArray) == true){
+//                 duplicates.push(input[i]);
+//                 break;
+//             } else {
+//                 uniqueArray.push(input[i]);
+//                 break;
+//             }
+//         }
+//     }
+// }
+// console.log(duplicates);
+
+
+// 14. Two Sum (VERY IMPORTANT)
+// let input = [2,7,11,15];
+// let target = 9;
+// let outPut = [];
+// for(let i = 0; i < input.length; i++){
+//     for(let x = 0; x < input.length; x++){
+//         if((input[i] + input[x]) == target && outPut.length == 0){
+//             outPut = [i, x];
+//         }
+//     }
+// }
+// console.log(outPut); // [0, 1]
