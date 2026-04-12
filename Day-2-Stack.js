c8213fb9ebb7d9637982d5073f4d9714128760b1
Kishan Kewal <iamdevildeveloper@gmail.com>
2026-04-12 20:10:48 +0530

    Learned about Stack

--- Day-2-Stack.js
+++ Day-2-Stack.js
@@ -27,32 +27,91 @@ class Stack {
     }
 }
 
-function removeAdjacentDuplicates(str) {
-    const stack = new Stack();
-    for(i = 0; i < str.length; i++){
-        const ch = str[i];
-        if(stack.isEmpty()){
-            stack.push(ch);
-        } else {
-            const top = stack.peek();
-            if(ch == top){
-                stack.pop();
-            } else {
-                stack.push(ch);
-            }
+function nextWarmingDay(temps) {
+    const stack = []; // will store indices
+    const result = new Array(temps.length).fill(0);
+    for (let i = 0; i < temps.length; i++) {
+        // while current temp is greater than stack top temp
+        while (stack.length && temps[i] > temps[stack[stack.length - 1]]) {
+            const prevIndex = stack.pop();
+            result[prevIndex] = i - prevIndex;
         }
+        stack.push(i);
     }
-    let normalString = "";
-    // FIX: use while instead of for
-    while (!stack.isEmpty()) {
-        normalString = stack.pop() + normalString; // reverse fix
-    }
-    return normalString;
+
+    return result;
 }
 
-const str = "abbaca";
-// const str = "abbacabasdd11opp";
-console.log(removeAdjacentDuplicates(str));
+const data = [73,74,75,71,69,72,76,73];
+console.log(nextWarmingDay(data)); // [1,1,4,2,1,1,0,0]
+
+
+// // 3. Next Greater Element
+// function nextGreaterElement(array) {
+//     const stack = new Stack();
+//     let stop = false; 
+//     let currentElement = null;
+//     let arrayCount = count = 0;
+
+//     while(stop == false){
+//         if(currentElement == null){
+//             currentElement = array[count];
+//         }
+//         const arrayVal = array[arrayCount];
+//         // console.log(currentElement, arrayCount, array, stack.size(), array.length);
+//         if(currentElement < arrayVal){
+//             // console.log(arrayVal);
+//             stack.push(arrayVal);
+//             currentElement = null;
+//             count++;
+//             arrayCount = 0;
+//         } else if (array.length == arrayCount) {
+//             stack.push(-1);
+//             arrayCount = 0;
+//         }
+//         arrayCount++;
+//         if(stack.size() == array.length) stop = true;
+//     }
+//     let result = [];
+//     const size = stack.size();
+//     for(i = 0; i < size; i++){
+//         result.unshift(stack.pop());
+//     }
+//     return result
+// }
+
+// // Input: [2,1,2,4,3]
+// // Output: [4,2,4,-1,-1]
+// const data = [2,1,2,4,3];
+// console.log(nextGreaterElement(data));
+
+
+// function removeAdjacentDuplicates(str) {
+//     const stack = new Stack();
+//     for(i = 0; i < str.length; i++){
+//         const ch = str[i];
+//         if(stack.isEmpty()){
+//             stack.push(ch);
+//         } else {
+//             const top = stack.peek();
+//             if(ch == top){
+//                 stack.pop();
+//             } else {
+//                 stack.push(ch);
+//             }
+//         }
+//     }
+//     let normalString = "";
+//     // FIX: use while instead of for
+//     while (!stack.isEmpty()) {
+//         normalString = stack.pop() + normalString; // reverse fix
+//     }
+//     return normalString;
+// }
+
+// const str = "abbaca";
+// // const str = "abbacabasdd11opp";
+// console.log(removeAdjacentDuplicates(str));
 
 // function isValidString(str){
 //     const stack = new Stack();
